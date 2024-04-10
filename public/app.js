27c6c40faa494c40310a717a394e4cf327ca7340
jmbartel02 <jbdino82@gmail.com>
2024-04-10 16:27:03 -0500

    Edit formatting

--- public/app.js
+++ public/app.js
@@ -22,7 +22,7 @@ officersData.forEach((officer) => {
   let card = document.createElement("div");
   card.className = "officer-card columns";
 
-  card.innerHTML = `<div class="column is-one-third"><figure class="image"><img class="officer-image" src="placeholder-image.jpg" alt="${officer.name}"></figure></div><div class="officer-info column"><h2 class="title is-4">${officer.name}</h2><h3 class="subtitle is-5">${officer.title}</h3><p><strong>Year:</strong> ${officer.year}</p><p><strong>Major:</strong>${officer.major}</p><p>${officer.bio}</p></div>`;
+  card.innerHTML = `<div class="column is-one-third"><figure class="image"><img class="officer-image" src="placeholder-image.jpg" alt="${officer.name}"></figure></div><div class="officer-info column"><h2 class="title is-4 has-text-danger-dark is-bold">${officer.name}</h2><h3 class="subtitle is-5">${officer.title}</h3><p><strong class="has-text-danger-dark">Year: </strong> ${officer.year}</p><p><strong class="has-text-danger-dark">Major: </strong>${officer.major}</p><p>${officer.bio}</p></div>`;
 
   officersContainer.appendChild(card);
 });
