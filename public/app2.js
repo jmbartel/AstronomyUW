0045f41ef165070b880c6886bf5800d8eca10298
Zach Knight <zknight@wisc.edu>
2024-04-13 00:14:38 -0500

    Remove redundant code

--- public/app2.js
+++ public/app2.js
@@ -8,11 +8,6 @@ let signinbtn = document.querySelector("#signin"); // good
 let signin_modal = document.querySelector("#signinmodal"); //good
 let signin_modalbg = document.querySelector("#signinmodal_bg"); //good
 
-// let postRecipeBtn = document.querySelector("#postRecipeBtn");
-
-// let hidden_form = document.querySelector("#hidden_form");
-// let content = document.querySelector("#content");
-
 function configure_message_bar(message) {
   // show a confirmation message for the user
 
@@ -35,8 +30,6 @@ function configure_message_bar(message) {
 function configure_nav_bar(user) {
   let signedinlinks = document.querySelectorAll(".signedin");
   let signedoutlinks = document.querySelectorAll(".signedout");
-
-  // console.log(signedinlinks, signedoutlinks);
 
   // check if user already exists
 
@@ -122,11 +115,7 @@ r_e("signin_form").addEventListener("submit", (e) => {
   let email = r_e("email_").value;
   let password = r_e("password_").value;
 
-  // console.log(email, password);
-
   auth.signInWithEmailAndPassword(email, password).then((user) => {
-    // console.log(user.user.email + " is signed up!");
-
     // clear the form
     // reset()
     hideModal_signin();
@@ -152,35 +141,13 @@ auth.onAuthStateChanged((user) => {
   if (user) {
     // configure the message bar
     UserEmail = user.email;
-    // configure_message_bar("Welcome back " + user.email);
-
-    // add user's email address to the nav bar
-
-    // r_e("currentuser").innerHTML = auth.currentUser.email;
 
     // configure nav bar
-
     configure_nav_bar(user);
-
-    // show all comments
-
-    // show_recipes(auth.currentUser.email);
   } else {
     // no user
     UserEmail = "";
-    // configure_message_bar("You are now signed out");
-    // r_e("currentuser").innerHTML;
-
-    // clear the user's email address from the nav bar
-
-    // r_e("currentuser").innerHTML = "";
-
-    // configure nav bar
-
     configure_nav_bar();
-
-    // call the show_recipes() with no arugment to hide all recipes
-    // show_recipes();
   }
 });
 
@@ -190,15 +157,10 @@ function r_e(id) {
 
 let signupmodal = document.querySelector("#signupmodal");
 let usersignup = document.querySelector("#signup");
-// 1. adding a reference to the modal background
+// adding a reference to the modal background
 let signupmodal_bg = document.querySelector("#signupmodal_bg");
-// console.log(signupmodal_bg);
-// console.log(signupmodal);
-// console.log(usersignup);
-// signupmodal.classList.add("is-active");
 
 function showmodal_signup() {
-  // alert("hello");
   // add the is-active class for the modal
   signupmodal.classList.add("is-active");
 }
@@ -207,12 +169,11 @@ usersignup.addEventListener("click", showmodal_signup);
 
 // hide the modal
 function hideModal_signup() {
-  // alert("hello");
-  // 3. once a modal background is clicked, remove 'is-active' from the modal
+  // once a modal background is clicked, remove 'is-active' from the modal
   signupmodal.classList.remove("is-active");
 }
 
-// 2. attach a click event on the modal background
+// attach a click event on the modal background
 signupmodal_bg.addEventListener("click", hideModal_signup);
 
 let loginmodal = r_e("signinmodal");
@@ -220,7 +181,6 @@ let usersignin = r_e("signin");
 let signinmodal_bg = r_e("signinmodal_bg");
 
 function showmodal_signin() {
-  // alert("hello");
   // add the is-active class for the modal
   loginmodal.classList.add("is-active");
 }
@@ -229,22 +189,12 @@ usersignin.addEventListener("click", showmodal_signin);
 
 // hide the modal
 function hideModal_signin() {
-  // alert("hello");
-  // 3. once a modal background is clicked, remove 'is-active' from the modal
+  // once a modal background is clicked, remove 'is-active' from the modal
   signinmodal.classList.remove("is-active");
 }
 
-// 2. attach a click event on the modal background
+// attach a click event on the modal background
 signinmodal_bg.addEventListener("click", hideModal_signin);
-
-// Leave modal when cancel is clicked
-// const signInButton = document.getElementById("signin");
-// const modal = document.getElementById("signinmodal");
-
-// signInButton.addEventListener("click", () => {
-//   // Show the modal
-//   modal.style.display = "block";
-// });
 
 // Cancel sign in
 const cancelsignin = document.getElementById("cancelsignin");
