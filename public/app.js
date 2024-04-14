54c3801c8638d12d72be166b78dd72561c3bdcb1
Jalen Corey <jalenmcorey@gmail.com>
2024-04-13 23:50:51 -0500

    Making website into single page application

--- public/app.js
+++ public/app.js
@@ -1,147 +1,239 @@
 // -------------------------------------------------------- AUTHENTICATION --------------------------------------------------------  //
 // global variables
 
-// let signinbtn = document.querySelector("#signin"); // good
-// let signin_modal = document.querySelector("#signinmodal"); //good
-// let signin_modalbg = document.querySelector("#signinmodal_bg"); //good
+let signinbtn = document.querySelector("#signin"); // good
+let signin_modal = document.querySelector("#signinmodal"); //good
+let signin_modalbg = document.querySelector("#signinmodal_bg"); //good
 
-// function configure_message_bar(message) {
-//   // show a confirmation message for the user
+function configure_message_bar(message) {
+  // show a confirmation message for the user
 
-//   // show the message bar for only 2 seconds and then hide it back
+  // show the message bar for only 2 seconds and then hide it back
 
-//   r_e("message_bar").classList.remove("is-hidden");
+  r_e("message_bar").classList.remove("is-hidden");
 
-//   r_e("message_bar").innerHTML = `${message}!`;
+  r_e("message_bar").innerHTML = `${message}!`;
 
-//   // hide the message bar after 2 seconds
+  // hide the message bar after 2 seconds
 
-//   setTimeout(() => {
-//     r_e("message_bar").classList.add("is-hidden");
+  setTimeout(() => {
+    r_e("message_bar").classList.add("is-hidden");
 
-//     // clear the message bar
-//     r_e("message_bar").innerHTML = "";
-//   }, 2000);
-// }
+    // clear the message bar
+    r_e("message_bar").innerHTML = "";
+  }, 2000);
+}
 
-// function configure_nav_bar(user) {
-//   let signedinlinks = document.querySelectorAll(".signedin");
-//   let signedoutlinks = document.querySelectorAll(".signedout");
+function configure_nav_bar(user) {
+  let signedinlinks = document.querySelectorAll(".signedin");
+  let signedoutlinks = document.querySelectorAll(".signedout");
 
-//   // check if user already exists
+  // check if user already exists
 
-//   if (user) {
-//     // show all elements with the class signedin
-//     // AND hide all elements with the class signedout
+  if (user) {
+    // show all elements with the class signedin
+    // AND hide all elements with the class signedout
 
-//     signedinlinks.forEach((link) => {
-//       link.classList.remove("is-hidden");
-//     });
+    signedinlinks.forEach((link) => {
+      link.classList.remove("is-hidden");
+    });
 
-//     signedoutlinks.forEach((link) => {
-//       link.classList.add("is-hidden");
-//     });
-//   } else {
-//     // no user
+    signedoutlinks.forEach((link) => {
+      link.classList.add("is-hidden");
+    });
+  } else {
+    // no user
 
-//     // hide all elements with the class signedin
-//     // AND show all elements with the class signedout
+    // hide all elements with the class signedin
+    // AND show all elements with the class signedout
 
-//     signedinlinks.forEach((link) => {
-//       link.classList.add("is-hidden");
-//     });
+    signedinlinks.forEach((link) => {
+      link.classList.add("is-hidden");
+    });
 
-//     signedoutlinks.forEach((link) => {
-//       link.classList.remove("is-hidden");
-//     });
-//   }
-// }
+    signedoutlinks.forEach((link) => {
+      link.classList.remove("is-hidden");
+    });
+  }
+}
 
-// // sign-in modal link
-// signinbtn.addEventListener("click", () => {
-//   signin_modal.classList.add("is-active");
-// });
+// sign-in modal link
+signinbtn.addEventListener("click", () => {
+  signin_modal.classList.add("is-active");
+});
 
-// signin_modalbg.addEventListener("click", () => {
-//   signin_modal.classList.remove("is-active");
-// });
+signin_modalbg.addEventListener("click", () => {
+  signin_modal.classList.remove("is-active");
+});
 
-// r_e("signin_form").addEventListener("submit", (e) => {
-//   // prevent the page from auto-refresh
-//   e.preventDefault();
+r_e("signin_form").addEventListener("submit", (e) => {
+  // prevent the page from auto-refresh
+  e.preventDefault();
 
-//   // get the email/password from the form
+  // get the email/password from the form
 
-//   let email = r_e("email_").value;
-//   let password = r_e("password_").value;
+  let email = r_e("email_").value;
+  let password = r_e("password_").value;
 
-//   auth.signInWithEmailAndPassword(email, password).then((user) => {
-//     // clear the form
-//     // reset()
-//     hideModal_signin();
-//     r_e("signin_form").reset();
+  auth.signInWithEmailAndPassword(email, password).then((user) => {
+    // clear the form
+    // reset()
+    hideModal_signin();
+    r_e("signin_form").reset();
 
-//     // close the modal
-//     // remove the is-active class from the modal
+    // close the modal
+    // remove the is-active class from the modal
 
-//     hideModal_signin();
-//   });
-// });
+    hideModal_signin();
+  });
+});
 
-// // sign out
+// sign out
 
-// r_e("signout").addEventListener("click", () => {
-//   auth.signOut().then(() => {});
-// });
+r_e("signout").addEventListener("click", () => {
+  auth.signOut().then(() => {});
+});
 
-// //newest
-// let UserEmail = "";
-// auth.onAuthStateChanged((user) => {
-//   // check if a user exists
-//   if (user) {
-//     // configure the message bar
-//     UserEmail = user.email;
+//newest
+let UserEmail = "";
+auth.onAuthStateChanged((user) => {
+  // check if a user exists
+  if (user) {
+    // configure the message bar
+    UserEmail = user.email;
 
-//     // configure nav bar
-//     configure_nav_bar(user);
-//   } else {
-//     // no user
-//     UserEmail = "";
-//     configure_nav_bar();
-//   }
-// });
+    // configure nav bar
+    configure_nav_bar(user);
+  } else {
+    // no user
+    UserEmail = "";
+    configure_nav_bar();
+  }
+});
 
-// function r_e(id) {
-//   return document.querySelector(`#${id}`);
-// }
+function r_e(id) {
+  return document.querySelector(`#${id}`);
+}
 
-// let loginmodal = r_e("signinmodal");
-// let usersignin = r_e("signin");
-// let signinmodal_bg = r_e("signinmodal_bg");
+let loginmodal = r_e("signinmodal");
+let usersignin = r_e("signin");
+let signinmodal_bg = r_e("signinmodal_bg");
 
-// function showmodal_signin() {
-//   // add the is-active class for the modal
-//   loginmodal.classList.add("is-active");
-// }
-// // show the modal
-// usersignin.addEventListener("click", showmodal_signin);
+function showmodal_signin() {
+  // add the is-active class for the modal
+  loginmodal.classList.add("is-active");
+}
+// show the modal
+usersignin.addEventListener("click", showmodal_signin);
 
-// // hide the modal
-// function hideModal_signin() {
-//   // once a modal background is clicked, remove 'is-active' from the modal
-//   signinmodal.classList.remove("is-active");
-// }
+// hide the modal
+function hideModal_signin() {
+  // once a modal background is clicked, remove 'is-active' from the modal
+  signinmodal.classList.remove("is-active");
+}
 
-// // attach a click event on the modal background
-// signinmodal_bg.addEventListener("click", hideModal_signin);
+// attach a click event on the modal background
+signinmodal_bg.addEventListener("click", hideModal_signin);
 
-// // Cancel sign in
-// const cancelsignin = document.getElementById("cancelsignin");
-// cancelsignin.addEventListener("click", hideModal_signin);
+// Cancel sign in
+const cancelsignin = document.getElementById("cancelsignin");
+cancelsignin.addEventListener("click", hideModal_signin);
+
+// -------------------------------------------------------- SINGLE-PAGE-APP --------------------------------------------------------  //
+// Selecting Home Page
+document.querySelector("#home_tab").addEventListener("click", () => {
+  document.querySelector("#home_page").classList.remove("is-hidden");
+  document.querySelector("#join_page").classList.add("is-hidden");
+  document.querySelector("#history_page").classList.add("is-hidden");
+  document.querySelector("#resources_page").classList.add("is-hidden");
+  document.querySelector("#events_page").classList.add("is-hidden");
+});
+
+// Selecting Join Page
+document.querySelector("#join_tab").addEventListener("click", () => {
+  document.querySelector("#join_page").classList.remove("is-hidden");
+  document.querySelector("#home_page").classList.add("is-hidden");
+  document.querySelector("#history_page").classList.add("is-hidden");
+  document.querySelector("#resources_page").classList.add("is-hidden");
+  document.querySelector("#events_page").classList.add("is-hidden");
+});
+
+// Selecting Events Page
+document.querySelector("#events_tab").addEventListener("click", () => {
+  document.querySelector("#events_page").classList.remove("is-hidden");
+  document.querySelector("#home_page").classList.add("is-hidden");
+  document.querySelector("#history_page").classList.add("is-hidden");
+  document.querySelector("#resources_page").classList.add("is-hidden");
+  document.querySelector("#join_page").classList.add("is-hidden");
+});
+
+// Selecting History Page
+document.querySelector("#history_tab").addEventListener("click", () => {
+  document.querySelector("#history_page").classList.remove("is-hidden");
+  document.querySelector("#home_page").classList.add("is-hidden");
+  document.querySelector("#events_page").classList.add("is-hidden");
+  document.querySelector("#resources_page").classList.add("is-hidden");
+  document.querySelector("#join_page").classList.add("is-hidden");
+});
+
+// Selecting Resources Page
+document.querySelector("#resources_tab").addEventListener("click", () => {
+  document.querySelector("#resources_page").classList.remove("is-hidden");
+  document.querySelector("#home_page").classList.add("is-hidden");
+  document.querySelector("#events_page").classList.add("is-hidden");
+  document.querySelector("#history_page").classList.add("is-hidden");
+  document.querySelector("#join_page").classList.add("is-hidden");
+});
+
+// APPLYING THE SAME EVENTS - FOOTER LINKS //
+// Selecting Home Page
+document.querySelector("#footer_home").addEventListener("click", () => {
+  document.querySelector("#home_page").classList.remove("is-hidden");
+  document.querySelector("#join_page").classList.add("is-hidden");
+  document.querySelector("#history_page").classList.add("is-hidden");
+  document.querySelector("#resources_page").classList.add("is-hidden");
+  document.querySelector("#events_page").classList.add("is-hidden");
+});
+
+// Selecting Join Page
+document.querySelector("#footer_join").addEventListener("click", () => {
+  document.querySelector("#join_page").classList.remove("is-hidden");
+  document.querySelector("#home_page").classList.add("is-hidden");
+  document.querySelector("#history_page").classList.add("is-hidden");
+  document.querySelector("#resources_page").classList.add("is-hidden");
+  document.querySelector("#events_page").classList.add("is-hidden");
+});
+
+// Selecting Events Page
+document.querySelector("#footer_events").addEventListener("click", () => {
+  document.querySelector("#events_page").classList.remove("is-hidden");
+  document.querySelector("#home_page").classList.add("is-hidden");
+  document.querySelector("#history_page").classList.add("is-hidden");
+  document.querySelector("#resources_page").classList.add("is-hidden");
+  document.querySelector("#join_page").classList.add("is-hidden");
+});
+
+// Selecting History Page
+document.querySelector("#footer_history").addEventListener("click", () => {
+  document.querySelector("#history_page").classList.remove("is-hidden");
+  document.querySelector("#home_page").classList.add("is-hidden");
+  document.querySelector("#events_page").classList.add("is-hidden");
+  document.querySelector("#resources_page").classList.add("is-hidden");
+  document.querySelector("#join_page").classList.add("is-hidden");
+});
+
+// Selecting Resources Page
+document.querySelector("#footer_resources").addEventListener("click", () => {
+  document.querySelector("#resources_page").classList.remove("is-hidden");
+  document.querySelector("#home_page").classList.add("is-hidden");
+  document.querySelector("#events_page").classList.add("is-hidden");
+  document.querySelector("#history_page").classList.add("is-hidden");
+  document.querySelector("#join_page").classList.add("is-hidden");
+});
 
 // -------------------------------------------------------- OFFICER DATA --------------------------------------------------------  //
 
-// // Sample officer data (replace with Firestore data)
+// Sample officer data (replace with Firestore data)
 // let officersData = [
 //   {
 //     name: "John Doe",
