12b17b3e6adb5e38523993df6bcc4d5cbcee70d8
Joey Bartel <jmbartel@wisc.edu>
2024-04-18 19:36:37 -0500

    bring back cards

--- public/app.js
+++ public/app.js
@@ -205,32 +205,32 @@ document.querySelector("#resources_tab").addEventListener("click", () => {
 // -------------------------------------------------------- OFFICER DATA --------------------------------------------------------  //
 
 // Sample officer data (replace with Firestore data)
-// let officersData = [
-//   {
-//     name: "John Doe",
-//     title: "President",
-//     year: "Senior",
-//     major: "Astronomy",
-//     bio: "Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
-//   },
-//   {
-//     name: "Jane Smith",
-//     title: "Vice President",
-//     year: "Junior",
-//     major: "Physics",
-//     bio: "Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris.",
-//   },
-// ];
+let officersData = [
+  {
+    name: "John Doe",
+    title: "President",
+    year: "Senior",
+    major: "Astronomy",
+    bio: "Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
+  },
+  {
+    name: "Jane Smith",
+    title: "Vice President",
+    year: "Junior",
+    major: "Physics",
+    bio: "Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris.",
+  },
+];
 
-// let officersContainer = document.getElementById("officers-container");
-// officersData.forEach((officer) => {
-//   let card = document.createElement("div");
-//   card.className = "officer-card columns";
+let officersContainer = document.getElementById("officers-container");
+officersData.forEach((officer) => {
+  let card = document.createElement("div");
+  card.className = "officer-card columns";
 
-//   card.innerHTML = `<div class="column is-one-third"><figure class="image"><img class="officer-image" src="placeholder-image.jpg" alt="${officer.name}"></figure></div><div class="officer-info column"><h2 class="title is-4 has-text-danger-dark is-bold">${officer.name}</h2><h3 class="subtitle is-5">${officer.title}</h3><p><strong class="has-text-danger-dark">Year: </strong> ${officer.year}</p><p><strong class="has-text-danger-dark">Major: </strong>${officer.major}</p><p>${officer.bio}</p></div>`;
+  card.innerHTML = `<div class="column is-one-third"><figure class="image"><img class="officer-image" src="placeholder-image.jpg" alt="${officer.name}"></figure></div><div class="officer-info column"><h2 class="title is-4 has-text-danger-dark is-bold">${officer.name}</h2><h3 class="subtitle is-5">${officer.title}</h3><p><strong class="has-text-danger-dark">Year: </strong> ${officer.year}</p><p><strong class="has-text-danger-dark">Major: </strong>${officer.major}</p><p>${officer.bio}</p></div>`;
 
-//   officersContainer.appendChild(card);
-// });
+  officersContainer.appendChild(card);
+});
 
 // --------------------------------------------------------  RESOURCES PAGE --------------------------------------------------------  //
 // Displaying and Hiding "Add Resources" Form //
