fdd898da511d9faca7b5a26f7f0bf5628959300f
grace <geow@wisc.edu>
2024-04-22 16:25:59 -0500

    committed

--- public/app.js
+++ public/app.js
@@ -242,193 +242,133 @@ let eventDateInput = document.getElementById("eventDate");
 let eventTitleInput = document.getElementById("eventName");
 let eventDescriptionInput = document.getElementById("eventDescription");
 let eventRsvpInput = document.getElementById("rsvpLink");
-let reminderList = document.getElementById("reminderList");
 
 // Counter to generate unique event IDs
 let eventIdCounter = 1;
 
 // Functions to add events
-// Function to display reminders
+function addEventToFirestore(event) {
+ let db = firebase.firestore();
+ let eventData = {
+   date: event.date,
+   title: event.title,
+   description: event.description,
+   rsvplink: event.rsvplink
+ };
+ 
+ db.collection("events").add(eventData)
+ .then(function(docRef){
+   console.log("Event added with ID: ", docRef.id);
+   event.firestoreId = docRef.id;
+ })
+ .catch(function(error) {
+   console.error('error adding event: ', error);
+ });
+}
+function addEvent() {
+ let dateInput = eventDateInput.value; // Get the date string from the input field
+ let dateParts = dateInput.split("-"); // Split the date string into parts
+ // Construct a date object in the local time zone
+ let date = new Date(dateParts[0], dateParts[1] - 1, dateParts[2]);
+ // Extract other event details
+ let title = eventTitleInput.value;
+ let description = eventDescriptionInput.value;
+ let rsvplink = eventRsvpInput.value;
+ if (title && date && !isNaN(date.getTime())) {
+   let eventId = eventIdCounter++;
+   let event = {
+     id: eventId,
+     date: date,
+     title: title,
+     description: description,
+     rsvplink: rsvplink,
+   };
+   addEventToFirestore(event);
+   events.push(event);
+   
+   showCalendar(currentMonth, currentYear);
+   eventDateInput.value = "";
+   eventTitleInput.value = "";
+   eventDescriptionInput.value = "";
+   eventRsvpInput.value = "";
+   displayReminders();
+ } else {
+   alert("Please enter a valid date.");
+ }
+}
+//Functions to delete events (locally and from database)
 function deleteEventFromFirestore(event){
-  let db = firebase.firestore();
+ let db = firebase.firestore();
 
-  db.collection("events")
-  .doc(event.firestoreId)
-  .delete()
-  .then(function() {
-    console.log("Event deleted from Firestore", event.firestoreId);
-  })
-  .catch(function(error) {
-    console.error("Error deleting from Firestore: ", error)
-  });
+ db.collection("events").doc(event.firestoreId).delete()
+ .then(function() {
+   console.log("Event deleted from Firestore");
+ })
+ .catch(function(error) {
+   console.error("Error deleting from Firestore: ", error)
+ });
 }
 
 function deleteEvent(eventId) {
-  let eventIndex = events.findIndex((event) => event.id === eventId);
-  if (eventIndex !== -1) {
-    // Remove the event from the events array
-    let deletedEvent = events.splice(eventIndex, 1)[0];
-    deleteEventFromFirestore(deletedEvent)
-      .then(function(){
-      console.log("Event deleted from Firestore database");
-      //refresh our website
-      showCalendar(currentMonth, currentYear);
-      displayReminders();
-    })
-      .catch(function(error) {
-        console.error("Error deleting event from Firestore: ", error);
-      });
+ let db = firebase.firestore();
 
-}} 
-function displayReminders() {
-  let reminderList = document.getElementById("reminderList");
-  reminderList.innerHTML = "";
-  for (let i = 0; i < events.length; i++) {
-    let event = events[i];
-    let eventDate = new Date(event.date);
-    // Adjust the month comparison to match the zero-based index
-    if (
-      eventDate.getMonth() + 1 === currentMonth + 1 && // Adding 1 to currentMonth to match the zero-based index
-      eventDate.getFullYear() === currentYear
-    ) {
-      let listItem = document.createElement("li");
-      listItem.innerHTML = `<strong>${event.title}</strong> - 
-            ${event.description} on 
-            ${eventDate.toLocaleDateString()} <a href="${event.rsvplink}" target="_blank">RSVP here</a>`;
+ db.collection("events").doc(eventId).delete()
+   .then(function() {
+     console.log("Event deleted from Firestore");
+ // Find the index of the event with the given ID
+ let eventIndex = events.findIndex((event) => event.id === eventId);
 
-      // Add a delete button for each reminder item
-      let deleteButton = document.createElement("button");
-      deleteButton.className = "button is-danger delete-event";
-      deleteButton.textContent = "Delete";
-      deleteButton.onclick = function () {
-        deleteEvent(event.id);
-      };
-      listItem.appendChild(deleteButton);
-      reminderList.appendChild(listItem);
-    }
-  }
+ if (eventIndex !== -1) {
+   // Remove the event from the events array
+   let deletedEvent = events.splice(eventIndex, 1)[0];
+   deleteEventFromFirestore(deletedEvent);
+   
+   showCalendar(currentMonth, currentYear);
+   displayReminders();
+ }
+}).catch(function(error) {
+ console.error("Error deleting event from Firestore: ", error);
+});
 }
-function addEventToFirestore(event) {
-  let db = firebase.firestore();
-  let eventData = {
-    date: event.date,
-    title: event.title,
-    description: event.description,
-    rsvplink: event.rsvplink
-  };
-  db.collection("events").add(eventData)
-  .then(function(docRef){
-    console.log("Event added with ID: ", docRef.id);
-    event.firestoreId = docRef.id;
-  })
-  .catch(function(error) {
-    console.error('error adding event: ', error);
-  });
-}
-function addEvent() {
-  let dateInput = eventDateInput.value; // Get the date string from the input field
-  let dateParts = dateInput.split("-"); // Split the date string into parts
-  // Construct a date object in the local time zone
-  let date = new Date(dateParts[0], dateParts[1] - 1, dateParts[2]);
-  // Extract other event details
-  let title = eventTitleInput.value;
-  let description = eventDescriptionInput.value;
-  let rsvplink = eventRsvpInput.value;
-  if (title && date && !isNaN(date.getTime())) {
-    let eventId = eventIdCounter++;
-    let event = {
-      id: eventId,
-      date: date,
-      title: title,
-      description: description,
-      rsvplink: rsvplink,
-    };
-    addEventToFirestore(event);
-    events.push(event);
-    
-    console.log("Events after adding:", events);
-    
-    showCalendar(currentMonth, currentYear);
-    eventDateInput.value = "";
-    eventTitleInput.value = "";
-    eventDescriptionInput.value = "";
-    eventRsvpInput.value = "";
-    
-    displayReminders();
-  } else {
-    alert("Please enter a valid date.");
-  }
-}
-//Functions to delete events (locally and from database)
-// function deleteEventFromFirestore(event){
-//   let db = firebase.firestore();
 
-//   db.collection("events").
-//   doc(event.firestoreId)
-//   .delete()
-//   .then(function() {
-//     console.log("Event deleted from Firestore", event.firestoreId);
-//   })
-//   .catch(function(error) {
-//     console.error("Error deleting from Firestore: ", error)
-//   });
-// }
-
-// function deleteEvent(eventId) {
-//   let eventIndex = events.findIndex((event) => event.id === eventId);
-//   if (eventIndex !== -1) {
-//     // Remove the event from the events array
-//     let deletedEvent = events.splice(eventIndex, 1)[0];
-//     deleteEventFromFirestore(deletedEvent)
-//       .then(function(){
-//       console.log("Event deleted from Firestore database");
-//       //refresh our website
-//       showCalendar(currentMonth, currentYear);
-//       displayReminders();
-//     })
-//       .catch(function(error) {
-//         console.error("Error deleting event from Firestore: ", error);
-//       });
-
-// }}
 // Function to display reminders
-// function displayReminders() {
-//   let reminderList = document.getElementById("reminderList");
-//   reminderList.innerHTML = "";
-//   for (let i = 0; i < events.length; i++) {
-//     let event = events[i];
-//     let eventDate = new Date(event.date);
-//     // Adjust the month comparison to match the zero-based index
-//     if (
-//       eventDate.getMonth() + 1 === currentMonth + 1 && // Adding 1 to currentMonth to match the zero-based index
-//       eventDate.getFullYear() === currentYear
-//     ) {
-//       let listItem = document.createElement("li");
-//       listItem.innerHTML = `<strong>${event.title}</strong> - 
-//             ${event.description} on 
-//             ${eventDate.toLocaleDateString()} <a href="${event.rsvplink}" target="_blank">RSVP here</a>`;
+function displayReminders() {
+ reminderList.innerHTML = "";
+ for (let i = 0; i < events.length; i++) {
+   let event = events[i];
+   let eventDate = new Date(event.date);
+   // Adjust the month comparison to match the zero-based index
+   if (
+     eventDate.getMonth() + 1 === currentMonth + 1 && // Adding 1 to currentMonth to match the zero-based index
+     eventDate.getFullYear() === currentYear
+   ) {
+     let listItem = document.createElement("li");
+     listItem.innerHTML = `<strong>${event.title}</strong> - 
+           ${event.description} on 
+           ${eventDate.toLocaleDateString()} <a href="${event.rsvplink}" target="_blank">RSVP here</a>`;
 
-//       // Add a delete button for each reminder item
-//       let deleteButton = document.createElement("button");
-//       deleteButton.className = "button is-danger delete-event";
-//       deleteButton.textContent = "Delete";
-//       deleteButton.onclick = function () {
-//         deleteEvent(event.id);
-//       };
-//       listItem.appendChild(deleteButton);
-//       reminderList.appendChild(listItem);
-//     }
-//   }
-// }}
+     // Add a delete button for each reminder item
+     let deleteButton = document.createElement("button");
+     deleteButton.className = "button is-danger delete-event";
+     deleteButton.textContent = "Delete";
+     deleteButton.onclick = function () {
+       deleteEvent(event.id);
+     };
+
+     listItem.appendChild(deleteButton);
+     reminderList.appendChild(listItem);
+   }
+ }
+}
 
 // Function to generate a range of
 // years for the year select input
 function generate_year_range(start, end) {
-  let years = "";
-  for (let year = start; year <= end; year++) {
-    years += "<option value='" + year + "'>" + year + "</option>";
-  }
-  return years;
+ let years = "";
+ for (let year = start; year <= end; year++) {
+   years += "<option value='" + year + "'>" + year + "</option>";
+ }
+ return years;
 }
 
 // Initialize date-related letiables
@@ -445,24 +385,24 @@ document.getElementById("year").innerHTML = createYear;
 let calendar = document.getElementById("calendar");
 
 let months = [
-  "January",
-  "February",
-  "March",
-  "April",
-  "May",
-  "June",
-  "July",
-  "August",
-  "September",
-  "October",
-  "November",
-  "December",
+ "January",
+ "February",
+ "March",
+ "April",
+ "May",
+ "June",
+ "July",
+ "August",
+ "September",
+ "October",
+ "November",
+ "December",
 ];
 let days = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
 
 $dataHead = "<tr>";
 for (dhead in days) {
-  $dataHead += "<th data-days='" + days[dhead] + "'>" + days[dhead] + "</th>";
+ $dataHead += "<th data-days='" + days[dhead] + "'>" + days[dhead] + "</th>";
 }
 $dataHead += "</tr>";
 
@@ -473,120 +413,120 @@ showCalendar(currentMonth, currentYear);
 
 // Function to navigate to the next month
 function next() {
-  currentYear = currentMonth === 11 ? currentYear + 1 : currentYear;
-  currentMonth = (currentMonth + 1) % 12;
-  showCalendar(currentMonth, currentYear);
+ currentYear = currentMonth === 11 ? currentYear + 1 : currentYear;
+ currentMonth = (currentMonth + 1) % 12;
+ showCalendar(currentMonth, currentYear);
 }
 
 // Function to navigate to the previous month
 function previous() {
-  currentYear = currentMonth === 0 ? currentYear - 1 : currentYear;
-  currentMonth = currentMonth === 0 ? 11 : currentMonth - 1;
-  showCalendar(currentMonth, currentYear);
+ currentYear = currentMonth === 0 ? currentYear - 1 : currentYear;
+ currentMonth = currentMonth === 0 ? 11 : currentMonth - 1;
+ showCalendar(currentMonth, currentYear);
 }
 
 // Adjusted Function to jump to a specific month and year
 function jump() {
-  currentYear = parseInt(document.getElementById("year").value);
-  currentMonth = parseInt(document.getElementById("month").value);
-  showCalendar(currentMonth, currentYear);
+ currentYear = parseInt(document.getElementById("year").value);
+ currentMonth = parseInt(document.getElementById("month").value);
+ showCalendar(currentMonth, currentYear);
 }
 // Adjusted Function to display the calendar
 function showCalendar(month, year) {
-  let firstDay = new Date(year, month, 1).getDay(); // Adjusted to get the correct first day of the week
-  let tbl = document.getElementById("calendar-body");
-  tbl.innerHTML = "";
-  let monthAndYear = document.getElementById("monthAndYear");
-  monthAndYear.innerHTML = months[month] + " " + year;
+ let firstDay = new Date(year, month, 1).getDay(); // Adjusted to get the correct first day of the week
+ let tbl = document.getElementById("calendar-body");
+ tbl.innerHTML = "";
+ let monthAndYear = document.getElementById("monthAndYear");
+ monthAndYear.innerHTML = months[month] + " " + year;
 
-  let date = 1;
-  for (let i = 0; i < 6; i++) {
-    let row = document.createElement("tr");
-    for (let j = 0; j < 7; j++) {
-      if (i === 0 && j < firstDay) {
-        let cell = document.createElement("td");
-        cell.innerHTML = "";
-        row.appendChild(cell);
-      } else if (date > daysInMonth(month, year)) {
-        break;
-      } else {
-        let cell = document.createElement("td");
-        cell.setAttribute("data-date", date);
-        cell.setAttribute("data-month", month + 1);
-        cell.setAttribute("data-year", year);
-        cell.setAttribute("data-month_name", months[month]);
-        cell.className = "date-picker";
-        cell.innerHTML = "<span>" + date + "</span>";
+ let date = 1;
+ for (let i = 0; i < 6; i++) {
+   let row = document.createElement("tr");
+   for (let j = 0; j < 7; j++) {
+     if (i === 0 && j < firstDay) {
+       let cell = document.createElement("td");
+       cell.innerHTML = "";
+       row.appendChild(cell);
+     } else if (date > daysInMonth(month, year)) {
+       break;
+     } else {
+       let cell = document.createElement("td");
+       cell.setAttribute("data-date", date);
+       cell.setAttribute("data-month", month + 1);
+       cell.setAttribute("data-year", year);
+       cell.setAttribute("data-month_name", months[month]);
+       cell.className = "date-picker";
+       cell.innerHTML = "<span>" + date + "</span>";
 
-        if (
-          date === today.getDate() &&
-          year === today.getFullYear() &&
-          month === today.getMonth()
-        ) {
-          cell.className = "date-picker selected";
-        }
+       if (
+         date === today.getDate() &&
+         year === today.getFullYear() &&
+         month === today.getMonth()
+       ) {
+         cell.className = "date-picker selected";
+       }
 
-        // Check if there are events on this date
-        if (hasEventOnDate(date, month, year)) {
-          cell.classList.add("event-marker");
-          cell.appendChild(createEventTooltip(date, month, year));
-        }
+       // Check if there are events on this date
+       if (hasEventOnDate(date, month, year)) {
+         cell.classList.add("event-marker");
+         cell.appendChild(createEventTooltip(date, month, year));
+       }
 
-        row.appendChild(cell);
-        date++;
-      }
-    }
-    tbl.appendChild(row);
-  }
+       row.appendChild(cell);
+       date++;
+     }
+   }
+   tbl.appendChild(row);
+ }
 
-  displayReminders();
+ displayReminders();
 }
 
 // Function to create an event tooltip
 // event tooltip is on the day (number-1)---> NEED TO FIX
 function createEventTooltip(date, month, year) {
-  let tooltip = document.createElement("div");
-  tooltip.classList.add(
-    "box",
-    "event-tooltip",
-    "has-background-link-light",
-    "has-text-black"
-  );
+ let tooltip = document.createElement("div");
+ tooltip.classList.add(
+   "box",
+   "event-tooltip",
+   "has-background-link-light",
+   "has-text-black"
+ );
 
-  let eventsOnDate = getEventsOnDate(date, month, year);
-  for (let i = 0; i < eventsOnDate.length; i++) {
-    let event = eventsOnDate[i];
-    let eventDate = new Date(event.date);
-    let eventText = `<strong>${event.title}</strong> - ${
-      event.description
-    } on ${eventDate.toLocaleDateString()}`;
-    let eventElement = document.createElement("p");
-    eventElement.innerHTML = eventText;
-    tooltip.appendChild(eventElement);
-  }
+ let eventsOnDate = getEventsOnDate(date, month, year);
+ for (let i = 0; i < eventsOnDate.length; i++) {
+   let event = eventsOnDate[i];
+   let eventDate = new Date(event.date);
+   let eventText = `<strong>${event.title}</strong> - ${
+     event.description
+   } on ${eventDate.toLocaleDateString()}`;
+   let eventElement = document.createElement("p");
+   eventElement.innerHTML = eventText;
+   tooltip.appendChild(eventElement);
+ }
 
-  return tooltip;
+ return tooltip;
 }
 // Function to get events on a specific date
 function getEventsOnDate(date, month, year) {
-  return events.filter(function (event) {
-    let eventDate = new Date(event.date);
-    return (
-      eventDate.getDate() === date &&
-      eventDate.getMonth() === month &&
-      eventDate.getFullYear() === year
-    );
-  });
+ return events.filter(function (event) {
+   let eventDate = new Date(event.date);
+   return (
+     eventDate.getDate() === date &&
+     eventDate.getMonth() === month &&
+     eventDate.getFullYear() === year
+   );
+ });
 }
 
 // Function to check if there are events on a specific date
 function hasEventOnDate(date, month, year) {
-  return getEventsOnDate(date, month, year).length > 0;
+ return getEventsOnDate(date, month, year).length > 0;
 }
 
 // Function to get the number of days in a month
 function daysInMonth(iMonth, iYear) {
-  return 32 - new Date(iYear, iMonth, 32).getDate();
+ return 32 - new Date(iYear, iMonth, 32).getDate();
 }
 
 // Call the showCalendar function initially to display the calendar
@@ -594,31 +534,29 @@ showCalendar(2, 2024);
 
 // display events already in firebase
 function loadEventsFromFirestore() {
-  let db = firebase.firestore();
+ let db = firebase.firestore();
 
-  db.collection("events").get()
-    .then(function(querySnapshot) {
-      querySnapshot.forEach(function(doc) {
-        let event = doc.data();
-        event.id = doc.id;
-        event.date = event.date.toDate(); // Convert Firestore Timestamp to JavaScript Date
-        events.push(event);
-      });
-      showCalendar(currentMonth, currentYear);
-      displayReminders();
-    })
-    .catch(function(error) {
-      console.error("Error loading events from Firestore: ", error);
-    });
+ db.collection("events").get()
+   .then(function(querySnapshot) {
+     querySnapshot.forEach(function(doc) {
+       let event = doc.data();
+       event.id = doc.id;
+       event.date = event.date.toDate(); // Convert Firestore Timestamp to JavaScript Date
+       events.push(event);
+     });
+     showCalendar(currentMonth, currentYear);
+     displayReminders();
+   })
+   .catch(function(error) {
+     console.error("Error loading events from Firestore: ", error);
+   });
 }
 
 document.addEventListener("DOMContentLoaded", function() {
-  loadEventsFromFirestore();
+ loadEventsFromFirestore();
 });
 
 //admin 
-
-
 
 // --------------------------------------------------------  RESOURCES PAGE --------------------------------------------------------  //
 // Displaying and Hiding "Add Resources" Form //
